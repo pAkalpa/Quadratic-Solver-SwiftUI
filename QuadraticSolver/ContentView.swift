5cd144d05a0acc0e1b36d89538f18a06ead91bce
pAkalpa <pasinduakalpa1998@gmail.com>
2023-02-24 10:12:46 +0530

    Initial Commit

---
+++ QuadraticSolver/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  QuadraticSolver
+//  Starter project code
+//  Created by Philip Trwoga on 09/02/2023.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    //state vars for a,b,c,x1, and x2
+    //data persistence
+    var body: some View {
+        ScrollView {
+            VStack {
+                //  Text(...)
+                // Label(...)
+                //Image(...)
+                //  TextField(...)
+                // etc.
+                
+                // Button(...)
+            }
+        }
+        //   Text(...)
+    }
+    func solveForX()
+    {
+        //  your code here
+        
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
